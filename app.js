9e5d372a484ea2f7a068050d9f3c18a4a939db7f
Polina <coozhukova@gmail.com>
2025-12-10 21:16:03 +0100

    done

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
+let name = "Polina";
+console.log("Hei " + name);
 
+let num1 = 4;
+let num2 = 3;
+console.log(num1 + " + " + num2 + " = " + (num1 + num2));
+console.log(num1 + " - " + num2 + " = " + (num1 - num2));
+console.log(num1 + " * " + num2 + " = " + (num1 * num2));
+console.log(num1 + " / " + num2 + " = " + (num1 / num2).toFixed(4));
+
+let age = 19;
+if (age < 18) {
+  console.log("Du får ikke kjøpe øl");
+} else {
+  console.log("Du får kjøpe øl");
+}
+
+let personAge = 45;
+if (personAge < 18) {
+  console.log("barnebillett");
+} else if (personAge >= 18 && personAge < 67) {
+  console.log("voksenbillett");
+} else {
+  console.log("honnørbillett");
+}
